ed496d59526398995b84f144a5632b969e9bc33a
Sebastian Luna-Valero <sebastian.luna.valero@gmail.com>
2023-06-07 08:49:36 +0200

    add public ssh key and floating ip

--- terraform/main.tf
+++ terraform/main.tf
@@ -33,11 +33,18 @@ resource "openstack_compute_secgroup_v2" "ssh" {
   }
 }
 
+# Public SSH key
+resource "openstack_compute_keypair_v2" "mykey" {
+  name       = "mykey"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC6NRGgoa77is6YJGo7xJP1awgMjTDC3b39vEfdTDuN85I8YAGluctwxZjCXTuCFJT6XpnD+8k6NFJytzbcX8ZsIwrFJEwW+xexbaYESnlDfYfF+tSyozMJFGCUzt9BC8tpPbK50gKNJ70rlx6I3biXA1JhhFOwVq661ISwID/akNqHb1lX4T4c9xGJKSm/GpN16KH43BtgXpdznQoVJIp/5cwWqJfmpSe2yVBZqJ3eISFlXXbMwuys8MEvyY0xQRvRIB5P+ACjRQyqrnGGP8rYwtVuvPkUrzrKuLK5J1p0xm4t5aYNGrKyL3WQkk1V4qNN+Acb/yQCyVeInJSE+4fF /home/sebastian/.ssh/id_rsa"
+}
+
 # VM
 resource "openstack_compute_instance_v2" "myvm" {
   name            = "Test VM"
   image_id        = data.openstack_images_image_v2.myimage.id
   flavor_id       = data.openstack_compute_flavor_v2.myflavor.id
+  key_pair        = openstack_compute_keypair_v2.mykey.name
   security_groups = ["ssh"]
   network {
     # need to look this up manually
@@ -45,3 +52,19 @@ resource "openstack_compute_instance_v2" "myvm" {
   }
 }
 
+# create floating IP
+resource "openstack_networking_floatingip_v2" "myvm_fip" {
+  # need to look this up manually
+  pool = "public-muni-147-251-21-GROUP"
+}
+
+# associate floating IP
+resource "openstack_compute_floatingip_associate_v2" "associate_myvm_fip" {
+  floating_ip = openstack_networking_floatingip_v2.myvm_fip.address
+  instance_id = openstack_compute_instance_v2.myvm.id
+}
+
+# print out the floating IP to connect to
+output "public_ip" {
+  value = openstack_networking_floatingip_v2.myvm_fip.address
+}
